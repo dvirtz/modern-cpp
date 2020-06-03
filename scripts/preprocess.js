b97b455f915ead3445c2487297316bf51b0c4e3a
dvirtz <dyitzchaki@oath.com>
2020-06-03 23:05:45 +0300

    enhance snippet tests

--- scripts/preprocess.js
+++ scripts/preprocess.js
@@ -1,20 +1,5 @@
-const { readFileSync } = require('fs');
-const path = require('path');
+const { readIndex } = require('./index-reader')
 
-const LINE_SEPARATOR = '\n';
-const FILE_REF_REGEX = /^FILE: (.+)$/;
-
-const isFileReference = (line) => FILE_REF_REGEX.test(line);
-const loadFileContent = (line, basePath) => {
-    const filePath = line.match(FILE_REF_REGEX)[1];
-
-    return readFileSync(path.join(basePath, filePath));
-};
-
-const preprocess = (markdown, options) =>
-    markdown
-        .split(LINE_SEPARATOR)
-        .map(line => isFileReference(line) ? loadFileContent(line, options.includeDir) : line)
-        .join(LINE_SEPARATOR);
+const preprocess = readIndex;
 
 module.exports = preprocess;
