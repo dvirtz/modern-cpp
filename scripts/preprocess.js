ae07c2c9119ffd81a264878822211dd1b7ddf626
dvirtz <dvirtz@gmail.com>
2020-10-27 13:35:44 +0200

    recursive preprocess

--- scripts/preprocess.js
+++ scripts/preprocess.js
@@ -1,5 +1,29 @@
-const { readIndex } = require('./index-reader')
+const { readFile } = require('fs').promises;
+const path = require('path');
 
-const preprocess = readIndex;
+const LINE_SEPARATOR = '\n';
+const FILE_REF_REGEX = /^FILE: (.+)$/;
+
+const loadFileContent = async (filePath, options) => {
+  const contents = await readFile(path.join(options.includeDir, filePath), 'utf-8');
+  if (path.extname(filePath) === '.md') {
+    return preprocess(contents, options);
+  }
+  return contents;
+};
+
+const processLine = async (line, options) => {
+  for (const m of (line.match(FILE_REF_REGEX) || []).slice(1)) {
+    return loadFileContent(m, options);
+  }
+
+  return Promise.resolve(line);
+}
+
+const preprocess = async (markdown, options) =>
+Promise.all(markdown
+  .split(LINE_SEPARATOR)
+  .map(line => processLine(line, options)))
+  .then(lines => lines.join(LINE_SEPARATOR));
 
 module.exports = preprocess;
--- test/test_code_snippets.js
+++ test/test_code_snippets.js
@@ -1,19 +1,18 @@
-const { fileList } = require('../scripts/index-reader');
 const { parseMarkdownSync, compile } = require('reveal-test');
 const assert = require('assert');
-const { readFileSync } = require('fs');
 const path = require('path');
 const { cwd } = require('process');
+const FileHound = require('filehound');
 
 const config = {
   compiler: 'g83'
 };
 
-fileList(readFileSync(path.join('slides', 'slides.md'), 'utf-8'))
+FileHound.create().path('slides').ext('md').findSync()
   .map(file => {
     return {
-      file: file,
-      snippets: parseMarkdownSync(path.join(cwd(), 'slides', file), config)
+      file: path.relative('slides', file),
+      snippets: parseMarkdownSync(path.join(cwd(), file), config)
     };
   })
   .filter(fileSnippets => fileSnippets.snippets.length > 0)
